543ac27dc68705825e2175516ee72bf7d28d1024
Prithvi <pritjam@gmail.com>
2023-01-31 22:35:41 -0500

    Started groundwork for memory savestating

--- lib/binloader.h
+++ lib/binloader.h
@@ -5,6 +5,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PARM_VERSION 1
+
+// The header for a pARMesan binary file.
+// This is kind of like an ELF file, in that it's
+// executable instructions in a file.
+typedef struct parmesan_rind {
+  char magic_identifier[4]; // should be "parm"
+  uint8_t version; 
+  uint8_t reserved[3]; // for future use
+} parmesan_rind_t;
+
+// Takes in a file pointer for a pARMesan binary file and loads it into memory starting at offset 0.
 extern void load_binary(uint16_t *emulated_mem, FILE *file);
+
+typedef struct memdump_header {
+  char magic_identifier[4]; // should be "Pmem"
+  uint8_t version; 
+  uint8_t num_of_segments; // how many segments are in this file
+  uint16_t reserved; // currently unused; might become a checksum or something else.
+} memdump_header_t;
+
+typedef struct memseg_header {
+  uint16_t start_address;
+  uint16_t length;
+  uint32_t checksum; // currently unimplemented, but might eventually be
+} memseg_header_t;
+
+// Takes in a file pointer for a pARMesan memory dump file and reads it into memory.
+// Offset and size information is contained within the pARMesan memory dump file header.
+extern void load_data_seg(uint16_t *emulated_mem, FILE *file);
 
 #endif
--- src/binloader.c
+++ src/binloader.c
@@ -1,6 +1,7 @@
 #include "binloader.h"
 
 #include "logging.h"
+#include <string.h>
 
 void load_binary(uint16_t *emulated_mem, FILE *file) {
   fseek(file, 0, SEEK_END); // seek to end of file
@@ -8,7 +9,7 @@ void load_binary(uint16_t *emulated_mem, FILE *file) {
   fseek(file, 0, SEEK_SET); // seek back to beginning of file
 
   // TODO: Implement asm file standards, checksum? header?
-  // for now, a basic test that the file length is a mult of 2B
+  // for now, a basic test that the file length is a mult of 2 Bytes
   if(size % 2 == 1) {
     log_msg(LOG_FATAL, "File does not meet correct standards!");
     exit(-1);
@@ -32,6 +33,49 @@ void load_binary(uint16_t *emulated_mem, FILE *file) {
     instr_word |= low_byte;
     emulated_mem[i / 2] = instr_word;
   }
+}
 
-  
+
+void load_data_seg(uint16_t *emulated_mem, FILE *file) {
+  memdump_header_t header;
+  // read in header
+  fread(&header, sizeof(memdump_header_t), 1, file);
+
+  // check magic identifier
+  if(!strncmp(header.magic_identifier, "parm", 4)) {
+    char temp[5];
+    strncpy(temp, header.magic_identifier, 4);
+    temp[4] = 0;
+    char msg[80];
+    sprintf(msg, "Bad file header for memdump file! Found %s, expected `Pmem`.", temp);
+    log_msg(LOG_FATAL, msg);
+    exit(EXIT_FAILURE);
+  }
+
+  // check version number
+  if(header.version != PARM_VERSION) {
+    char msg[70];
+    sprintf(msg, "Bad file version for memdump file! Found %d, expected %d.", header.version, PARM_VERSION);
+    log_msg(LOG_FATAL, msg);
+    exit(EXIT_FAILURE);
+  }
+
+  // If both version number and magic identifier worked out:
+  // we are ready to start reading memory
+  memseg_header_t segment;
+
+  for(int i = 0; i < header.num_of_segments; i++) {
+    // read segment header
+    fread(&segment, sizeof(memseg_header_t), 1, file);
+    // read in the segment as bytes first
+    fread(emulated_mem + segment.start_address, sizeof(uint8_t), 2 * segment.length, file);
+    // TODO: checksum calculation would happen here
+    // correct endianness
+    uint8_t *mem_byte_ptr = (uint8_t *) emulated_mem;
+    for(int i = segment.start_address; i < segment.start_address + segment.length; i++) {
+      uint16_t high_byte = *(mem_byte_ptr + 2 * i);
+      uint16_t low_byte = *(mem_byte_ptr + 2 * i + 1);
+      emulated_mem[i] = (high_byte << 8) | low_byte;
+    }
+  }
 }
