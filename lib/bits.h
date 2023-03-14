eecd0d1fb0370869f6a2166df783e576bfdf1f67
Prithvi <pritjam@gmail.com>
2023-03-13 21:41:32 -0400

    added comments for documentation

--- lib/bits.h
+++ lib/bits.h
@@ -3,12 +3,28 @@
 
 #include <stdint.h>
 
+
+/**
+* Extract a bitfield from an integer.
+* @param srcbits the uint16 from which to extract bits.
+* @param from the bit-offset to start extracting bits at (inclusive)
+* @param width the number of bits to extract
+* @return an unsigned integer with value equal to that of the extracted bitfield
+*/
 inline static uint16_t extract_unsigned_immediate(uint16_t srcbits, int from, int width) {
   srcbits >>= from;
   unsigned mask = (1 << width) - 1;
   return srcbits & mask;
 }
 
+
+/**
+* Extract a bitfield from an integer, sign-extending if necessary
+* @param srcbits the uint16 from which to extract bits.
+* @param from the bit-offset to start extracting bits at (inclusive)
+* @param width the number of bits to extract
+* @return an unsigned integer with value equal to that of the extracted bitfield in twos-complement representation.
+*/
 inline static uint16_t extract_signed_immediate(uint16_t srcbits, int from, int width) {
   uint16_t unsigned_imm = extract_unsigned_immediate(srcbits, from, width);
   // now we have to sign extend that
--- lib/logging.h
+++ lib/logging.h
@@ -9,14 +9,21 @@ extern int plain_print;
 
 /* This enum represents the various levels used by the logging system. */
 typedef enum {
-  LOG_DEBUG, // Debug messages (specifically for debugging)
-  LOG_INFO,  // Info messages (startup, loading, etc)
-  LOG_WARN,  // Warnings (nullptr reads, simultaneous r/w, etc)
-  LOG_FATAL, // Unrecoverable errors (terminates execution)
-  LOG_OUTPUT,// Program output (yet to be implemented)
-  LOG_OTHER = -1 // should not be used
+  LOG_DEBUG,      // Debug messages (specifically for debugging)
+  LOG_INFO,       // Info messages (startup, loading, etc)
+  LOG_WARN,       // Warnings (nullptr reads, simultaneous r/w, etc)
+  LOG_FATAL,      // Unrecoverable errors (terminates execution)
+  LOG_OUTPUT,     // Program output
+  LOG_OTHER = -1  // should not be used
 } log_level;
 
+/**
+* Log a message to the console.
+* Uses the global variable global_verbosity to filter logs that don't meet the verbosity threshold.
+* Also uses the global variable plain_print to determine if color formatting is to be used.
+* @param lvl the log level to use, such as LOG_INFO or LOG_FATAL
+* @param msg a pointer to the message to log.
+*/
 extern void log_msg(log_level lvl, char *msg);
 
 #endif
